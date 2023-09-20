a48508209386b2e81b72e135f8ec711386c80c6f
Joaquinmv1 <villarrealjoaquin09@gmail.com>
2023-09-20 18:46:55 -0300

    created chat

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,14 +1,62 @@
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+
+const URL = 'http://localhost:3000';
+
+const socket = io(URL);
+
 export default function Home() {
+  const [messages, setMessages] = useState<Array<string>>([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setMessages([...messages, newMessage]);
+    setNewMessage('');
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewMessage(e.target.value);
+  };
+
+  const sendMessage = (message: string) => {
+    setMessages((prevState) => [...prevState, message]);
+  }
+
+  useEffect(() => {
+    socket.on('chat-message', sendMessage);
+
+    return () => {
+      socket.off('chat-message', sendMessage);
+    };
+  }, []);
+
   return (
-    <section className="h-screen flex items-center justify-center w-full">
-      <div className="grid grid-cols-2 gap-10 w-96">
-        <div>
-          contacts
+    <section className="min-h-screen flex items-center justify-center">
+      <article className='bg-gray-700'>
+        <div className="p-10">
+          <h3 className="font-bold">Conversations</h3>
+          <form onSubmit={handleSubmit}>
+            <input
+              onChange={handleChange}
+              value={newMessage}
+              className="bg-zinc-600 placeholder-white py-1 px-2 rounded" type="text" placeholder="Search"
+            />
+          </form>
+          <div className="flex">
+            <p>Recent Chats</p>
+            <p>+ New Chat</p>
+          </div>
         </div>
         <div>
-          chat
+          <ul>
+            {messages.map((message, i) => (
+              <li key={`${message} ${i}`}>{message}</li>
+            ))}
+          </ul>
         </div>
-      </div>
+      </article>
     </section>
   )
 }
